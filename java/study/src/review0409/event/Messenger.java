dad81f3572b9c9ac22f83068bf9f6cad6919b4b5
onuo77 <onuo77@gmail.com>
2021-05-12 22:26:37 +0900

    210512

--- java/study/src/review0409/event/Messenger.java
+++
@@ -1,6 +0,0 @@
-package review0409.event;
-
-public interface Messenger {
-
-	void send(String to, String from, String text);
-}
--- java/study/src/review0409/event/SNS.java
+++
@@ -1,6 +0,0 @@
-package review0409.event;
-
-public interface SNS {
-
-	void post(String title, String writer, String content);
-}
